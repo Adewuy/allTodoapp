e92baf3d94ff984b0952ee46ef1275d088d7cf78
Adewuy <106016379+Adewuy@users.noreply.github.com>
2024-01-11 21:07:27 +0100

    todo-app upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (input.trim() !== "") {
+      const newTodo = {
+        id: Math.random(),
+        todo: input,
+        checked: false,
+      };
+
+      setList([...list, newTodo]);
+      setInput("");
+    }
+  };
+
+  const handleToggleTodo = (index) => {
+    const updatedList = list.map((todo, i) =>
+      i === index ? { ...todo, checked: !todo.checked } : todo
+    );
+
+    setList(updatedList);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const updatedList = list.filter((todo) => todo.id !== id);
+
+    setList(updatedList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo list</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {list.map((todo, index) => (
+          <li key={index} style={{ display: "flex" }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <input
+                type="checkbox"
+                checked={todo.checked}
+                onChange={() => handleToggleTodo(index)}
+              />
+              {todo.todo}
+              <button style={{marginTop: "5px", marginBottom: "5px"}} onClick={() => handleDeleteTodo(todo.id)}>
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
